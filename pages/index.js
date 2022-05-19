3d0d00eb56703cdaf1607ec4579ca940f73788e6
ishe96 <amadamombe904@gmail.com>
2022-05-19 23:30:08 +0200

    sanity studio + tokens

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,42 @@
 // import styles from '../styles/Home.module.css'
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { AiOutlinePlus } from "react-icons/ai";
+import Header from "../components/Header";
+import PortfolioChart from "../components/PortfolioChart";
+import BuyTokens from "../components/BuyTokens";
+import Asset from "../components/Assets";
+import Notice from "../components/Notice";
 
 const styles = {
     wrapper: "w-screen h-screen flex flex-col",
     mainContainer: "w-2/3 h-full m-auto flex mt-16",
+    leftMain: "flex flex-col w-3/4 h-full p-6 overflow-y-scroll",
+    portfolioAmountContainer: "flex flex-col",
+    portfolioAmount: "text-gray-400 text-4xl",
+    portfolioPercent: "text-gray-400 font-bold text-sm",
+    pastHour: "text-gray-400",
+    chartContainer:
+        "text-5xl flex justify-center w-full h-1/3 text-white mt-11 mb-11",
+    buyingPowerContainer:
+        "w-full border-t mb-24 border-b h-16 border-[#30363b] flex justify-between items-center p-4",
+    buyingPowerTitle: "text-gray-400 font-bolder text-lg",
+    buyingPowerAmount: "text-gray-400 font-bolder text=xl",
+    notice: "flex border border-[#30363b] mx-11 my-4 p-5 flex-col flex-1",
+    noticeContainer: "flex-1",
+    noticeTitle: "text-gray-500",
+    noticeMessage: "text-gray-400 font-bold",
+    noticeCTA: "font-bold text-green-500 cursor-pointer mt-5",
+    rightMain:
+        "flex flex-col flex-1 h-4/5 bg-[#1e2123] mt-6 rounded-lg overflow-y-scroll noscroll",
+    rightMainItem: "flex items-center text-white p-5 border-b border-[#30363b]",
+    ItemTitle: "flex font-bold",
+    moreOptions: "cursor-pointer text-xl",
 };
 
 export default function Home() {
     return (
         <div className={styles.wrapper}>
-            {/* Header */}
+            <Header/>
 
             <div className={styles.mainContainer}>
                 <div className={styles.leftMain}>
@@ -22,40 +48,47 @@ export default function Home() {
                         </div>
                     </div>
                     <div className={styles.chartContainer}>
-                        {/* <PortfolioChart/> */}
+                        <PortfolioChart/>
                     </div>
-                </div>
 
-                <div className={styles.buyingPower}>
-                    <div className={styles.buyingPowerTitle}>Buying Power</div>
-                    <div className={styles.buyingPowerAmount}>15 ETH</div>
-                </div>
-
-                <div className={styles.notice}>
-                    <div className={styles.noticeContainer}>
-                        <div className={styles.noticeTitle}>Send Funds</div>
-                        <div className={styles.noticeMessage}>
-                            Transaction transfer
+                    <div className={styles.buyingPowerContainer}>
+                        <div className={styles.buyingPowerTitle}>
+                            Buying Power
                         </div>
-                        {/* <BuyTokens/> */}
+                        <div className={styles.buyingPowerAmount}>15 ETH</div>
+                    </div>
+
+                    <div className={styles.notice}>
+                        <div className={styles.noticeContainer}>
+                            <div className={styles.noticeTitle}>Send Funds</div>
+                            <div className={styles.noticeMessage}>
+                                Transaction transfer
+                            </div>
+                            <BuyTokens/>
+                        </div>
+                    </div><Notice/>
+                </div>
+
+                {/* </div> */}
+                
+
+                <div className={styles.rightMain}>
+                    <div className={styles.rightMainItem}>
+                        <div className={styles.ItemTitle}>
+                            Crypto Currencies
+                        </div>
+
+                        <BiDotsHorizontalRounded
+                            className={styles.moreOptions}
+                        />
+                    </div>
+                    {/* Map through coins and for every coin make an Assert */}
+                    <Asset coin={'ETH'} price={'82'} />
+                    <div className={styles.rightMainItem}>
+                        <div className={styles.ItemTitle}>Lists</div>
+                        <AiOutlinePlus className={styles.moreOptions} />
                     </div>
                 </div>
-            {/* </div> */}
-            {/* <Notifier/> */}
-            <div className={styles.rightMain}>
-                <div className={styles.rightMainItem}>
-                    <div className={styles.itemTitle}>Crypto Currencies</div>
-
-                    <BiDotsHorizontalRounded className={styles.moreOptions} />
-                </div>
-                {/* Map through coins and for every coin make an Assert */}
-                {/* <Asset/> */}
-
-                <div className={styles.rightMainItem}>
-                    <div className={styles.itemTitle}>Lists</div>
-                    <AiOutlinePlus className={styles.moreOptions} />
-                </div>
-            </div>
             </div>
         </div>
     );
